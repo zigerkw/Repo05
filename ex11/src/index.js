e12b50fa8b2f57c4902801564894dccc8961ffb2
zigerkw <zejdhadzic@hotmail.com>
2021-09-17 14:37:53 +0300

    jeanaesta vjezba

---
+++ ex11/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Tesla";
+    var thirdNameOfLastName = lastName[2];
+    // Only change code above this line
+
+    return thirdNameOfLastName
+}
+
+console.log(main());
+module.exports = main;
