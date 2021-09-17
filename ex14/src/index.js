425b9f205502845bb3a538c92719292dae99f229
zigerkw <zejdhadzic@hotmail.com>
2021-09-17 15:36:19 +0300

    cetrnaesta vjezba

---
+++ ex14/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog ";
+    var myVerb = "run ";
+    var myAdjective = "big ";
+    var myAdverb = "quickly";
+    var wordBlanks = myAdjective + myNoun + myVerb + myAdverb + "."
+
+    // Only change code above this line
+
+    return wordBlanks
+}
+
+console.log(main());
+module.exports = main;
