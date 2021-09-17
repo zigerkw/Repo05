a7887db90ac6ee11d9b0cc41215591ac1bf4ba06
zigerkw <zejdhadzic@hotmail.com>
2021-09-17 13:59:05 +0300

    osma vjezba

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var LastName = "Doe";
+    lastNameLength = LastName.length;
+    // Only change code above this line
+
+    return lastNameLength
+}
+
+console.log(main());
+module.exports = main;
